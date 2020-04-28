92cae4ce965ff6762d203455f0dc89816bd2e87e
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2020-04-28 13:32:50 +0300

    Variable with clear context

---
+++ Chapter2/2_2VariableWithClearContext.rb
@@ -0,0 +1,42 @@
+# In order to run the make function from command line do:
+# ruby -r "./2_2VariableWithClearContext.rb" -e "GuessStatisticsMessage.make 'd','3'"
+
+class GuessStatisticsMessage
+$number = '';
+$verb = '';
+$pluralModifier = '';
+
+def self.make(candidate, count)
+createPluralDependentMessageParts(count);
+puts "There #{$verb} #{$number} #{candidate} #{$pluralModifier}"
+end
+
+def self.createPluralDependentMessageParts(count)
+if count == 0
+    thereAreNoLetters();
+elsif count == 1
+    thereIsOneLetter();
+else
+    thereAreManyLetters(count);
+end
+end
+
+def self.thereAreManyLetters(count)
+$number = count.to_s;
+$verb = "are";
+$pluralModifier = "s";
+end
+
+def self.thereIsOneLetter()
+$number = "1";
+$verb = "is";
+$pluralModifier = "";
+end
+
+def self.thereAreNoLetters()
+$number = "no";
+$verb = "are";
+$pluralModifier = "s";
+end
+
+end
